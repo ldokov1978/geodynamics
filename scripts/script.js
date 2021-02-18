c4d86c3343002aad2ba350a0222c0e0325de4478
Denis <ldokov@mail.ru>
2021-02-18 23:09:45 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,8 @@ $(document).ready(function () {
             easeLinearity: 0.25,
             noMoveStart: false
         });
-
+    }
+               
     function popUp(f, l) {
         var out = [];
         if (f.properties) {
